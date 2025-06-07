f8a7255207427951c56e90b14d808bc421a49bab
tanimomor <omoraltanim@gmail.com>
2025-06-07 21:23:56 +0600

    docs: Add comprehensive README for localization project
    - Add detailed project documentation and setup instructions
    - Include features list and supported languages
    - Document localization configuration and implementation
    - Provide code examples for view-based localization
    - Add instructions for extending language support

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "en-US", "de-DE" };
+    options.SetDefaultCulture(supportedCultures[1]);
+    options.SupportedCultures = supportedCultures.Select(c => new CultureInfo(c)).ToList();
+    options.SupportedUICultures = options.SupportedCultures;
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix );
+
+var app = builder.Build();
+app.UseRequestLocalization();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
